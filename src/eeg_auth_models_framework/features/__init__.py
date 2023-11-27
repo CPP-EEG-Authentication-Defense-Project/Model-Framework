22087e870772223a0a4075e9f1aaa4e8f6881be2
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2023-11-26 18:09:09 -0800

    Add top level import for features package

--- src/eeg_auth_models_framework/features/__init__.py
+++ src/eeg_auth_models_framework/features/__init__.py
@@ -2,3 +2,4 @@ from .base import FeatureExtractor
 from .ar import ARFeatureExtractor
 from .statistical import StatisticalFeature, StatisticalFeatureExtractor
 from .pipeline import FeatureExtractPipeline
+from .power import PSDExtractor
