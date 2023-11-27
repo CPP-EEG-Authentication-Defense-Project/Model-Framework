1cfb8f4859c3b3f523787b75e62715edfd4a0092
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2023-11-26 18:33:55 -0800

    Make channels list optional

--- src/eeg_auth_models_framework/utils/conversion.py
+++ src/eeg_auth_models_framework/utils/conversion.py
@@ -9,7 +9,7 @@ class MNEDataFrameConverter:
     Utility class which is used to create an object capable of repeated conversions from Pandas DataFrame format
     to MNE data format.
     """
-    def __init__(self, channels: typing.List[str], sample_frequency: int):
+    def __init__(self, sample_frequency: int, channels: typing.List[str] = None):
         self.channels = channels
         self.sample_frequency = sample_frequency
 
@@ -20,6 +20,7 @@ class MNEDataFrameConverter:
         :param dataframe: the Pandas DataFrame to convert.
         :return: the MNE format data.
         """
+        channels = self.channels or dataframe.columns.values.tolist()
         transposed_dataframe = dataframe.transpose(copy=True)
-        data_info = mne.create_info(self.channels, self.sample_frequency, ch_types='eeg')
+        data_info = mne.create_info(channels, self.sample_frequency, ch_types='eeg')
         return mne.io.RawArray(transposed_dataframe.to_numpy(), data_info)
