9ad6be6b4308e7db741b4678b88a2ee235fefcf6
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2024-01-05 21:33:15 -0800

    Implement median and min-max normalization

--- src/eeg_auth_models_framework/normalization/base.py
+++ src/eeg_auth_models_framework/normalization/base.py
@@ -12,9 +12,14 @@ class FeatureMetaData:
     such as quantization.
     """
     std_dev: float
+    mean_abs_dev: float
     mean: float
+    median: float
     min: float
     max: float
+
+
+AttributeOption = typing.Literal['std_dev', 'mean_abs_dev', 'mean', 'median', 'min', 'max']
 
 
 class FeatureMetaDataIndex(list[FeatureMetaData]):
@@ -22,7 +27,7 @@ class FeatureMetaDataIndex(list[FeatureMetaData]):
     Specialized list type used to create an index of feature vector metadata.
     """
     @functools.lru_cache(maxsize=4)
-    def get_metadata_vector(self, attr: typing.Literal['std_dev', 'mean', 'min', 'max']) -> np.ndarray:
+    def get_metadata_vector(self, attr: AttributeOption) -> np.ndarray:
         """
         Generates a 1-D vector of metadata based on the given metadata attribute.
 
