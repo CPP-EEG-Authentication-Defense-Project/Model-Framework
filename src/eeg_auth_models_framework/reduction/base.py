d07fed90378cb7b4947d3c00514bfae5ab0dc3b0
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2024-01-12 19:28:08 -0800

    Implement averaging feature reduction

--- src/eeg_auth_models_framework/reduction/base.py
+++ src/eeg_auth_models_framework/reduction/base.py
@@ -3,13 +3,13 @@ import typing
 import numpy as np
 
 
-class FeatureDataReducer(abc.ABC):
+class FeatureReduction(abc.ABC):
     """
     Base interface for a feature reduction processor. The purpose of such objects is to take in a series of feature
     data vectors and reduce them into a single feature vector.
     """
     @abc.abstractmethod
-    def reduce(self, data: typing.Iterable[np.ndarray]) -> np.ndarray:
+    def reduce(self, data: typing.List[np.ndarray]) -> np.ndarray:
         """
         Reduces the given series of feature vectors to a single feature vector.
 
