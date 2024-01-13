fee1314b9a69df00f17d1e7ff520e4e5af8017f0
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2024-01-12 20:37:00 -0800

    Switch concatenate to vstack

--- src/eeg_auth_models_framework/reduction/average.py
+++ src/eeg_auth_models_framework/reduction/average.py
@@ -10,6 +10,6 @@ class AverageFeatureReduction(base.FeatureReduction):
     Reduces feature data vectors by averaging all features across each feature index (i.e., each column in the matrix).
     """
     def reduce(self, data: typing.List[np.ndarray]) -> np.ndarray:
-        dataframe = pd.DataFrame(np.concatenate(data))
+        dataframe = pd.DataFrame(np.vstack(data))
         average_data = dataframe.mean()
         return average_data.to_numpy()
