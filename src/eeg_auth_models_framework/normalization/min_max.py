080b5fc283dfab6e8a657f441598e676abf860ed
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2024-01-12 20:52:49 -0800

    Set metadata requirement flags

--- src/eeg_auth_models_framework/normalization/min_max.py
+++ src/eeg_auth_models_framework/normalization/min_max.py
@@ -5,5 +5,5 @@ class MinMaxNormalizationStep(rescale.RescaleNormalizationStep):
     """
     Simple alias for rescaling data to a minimum and maximum of 0 and 1, respectively.
     """
-    def __init__(self, metadata: base.FeatureMetaDataIndex):
-        super().__init__(metadata, lower=0, upper=1.0)
+    def __init__(self):
+        super().__init__(lower=0, upper=1.0)
--- src/eeg_auth_models_framework/normalization/norm.py
+++ src/eeg_auth_models_framework/normalization/norm.py
@@ -8,6 +8,8 @@ class L1NormalizationStep(base.NormalizationStep):
     L1 normalization approach, based on the description from Lee Friedman and Oleg V. Komogortsev
     in their paper on biometric feature normalization techniques (DOI: 10.1109/TIFS.2019.2904844).
     """
+    metadata_required = True
+
     def normalize(self, data: np.ndarray) -> np.ndarray:
         mean_centered_data = data - self.metadata.get_metadata_vector('mean')
         normalized_vector = mean_centered_data / np.linalg.norm(mean_centered_data, ord=1)
